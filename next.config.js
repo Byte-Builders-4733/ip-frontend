a9acb17621cb4a80fede977d8a89283dec74d252
TemaSaur <69428015+TemaSaur@users.noreply.github.com>
2023-06-09 21:32:01 +0500

    add redirect to backend admin

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
 	env: {
 		API: process.env.API,
-	}
+	},
+	async redirects() {
+		return [
+			{
+				source: "/admin",
+				destination: "https://mxoil1.pythonanywhere.com/admin",
+				permanent: true,
+			},
+		];
+	},
 }
 
 module.exports = nextConfig
